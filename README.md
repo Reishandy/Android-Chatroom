9a4103fce11246de2341b52d2c6e15698f8ce0c7
Reishandy <isthisruxury@gmail.com>
2025-03-13 20:09:24 +0700

    Project init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rei's Chatroom
+
+TODO
+
+## Features
+
+TODO
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
+
+
+## Contributing
+
+Contributions are welcome! If you have ideas for new features or improvements.
+
+## 📄 License
+
+This project is licensed under the AGPL-3.0 License - see the [LICENSE](LICENSE) file for details.
+
+## 🙏 Credits
+
+Created by [Reishandy](https://github.com/Reishandy)
+
---
+++ app/src/main/java/com/reishandy/chatroom/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.reishandy.chatroom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.reishandy.chatroom.ui.theme.ChatroomTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatroomTheme {
+                // TODO TEST
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Card(
+                            modifier = Modifier,
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer
+                            )
+
+                        ) {
+                            Text("Hello, Chatroom!", modifier = Modifier.padding(16.dp), color = MaterialTheme.colorScheme.onPrimary)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
